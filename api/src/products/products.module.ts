d4df124cd396b0b3eb2bf94b6dc7ba4eae5d2cca
owtdxv <owtdxv@gmail.com>
2025-05-30 11:35:20 +0900

    feat: 최근 본 상품, 좋아요 표시한 상품을 반환하는 서비스 함수 추가

--- api/src/products/products.module.ts
+++ api/src/products/products.module.ts
@@ -4,10 +4,15 @@ import { ProductsService } from './products.service';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from 'src/common/schemas/product.schema';
+import { UserProductListSchema } from 'src/common/schemas/user-product-lists.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: 'LikedProducts', schema: UserProductListSchema },
+      { name: 'ViewedProducts', schema: UserProductListSchema },
+    ]),
   ],
   controllers: [ProductsController],
   providers: [ProductsService, JwtStrategy],
--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -3,11 +3,16 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Product, ProductDocument } from 'src/common/schemas/product.schema';
+import { UserProductListDocument } from 'src/common/schemas/user-product-lists.schema';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+    @InjectModel('LikedProducts')
+    private readonly likedProductsModel: Model<UserProductListDocument>,
+    @InjectModel('ViewedProducts')
+    private readonly viewedProductsModel: Model<UserProductListDocument>,
     private jwtService: JwtService,
   ) {}
 
@@ -63,5 +68,91 @@ export class ProductsService {
     }
 
     return await query.exec();
+  }
+
+  /**
+   * 사용자가 좋아요 표시한 상품 정보를 filter조건에 맞게 반환합니다
+   * @param uid 사용자 uid
+   * @param filter 최신순(default), 낮은가격순, 높은가격순
+   */
+  async getLikedProduct(
+    uid: string,
+    filter: string = '최신순',
+  ): Promise<Product[]> {
+    let sortOption: Record<string, 1 | -1> = { createdAt: -1 }; // 기본값: 상품 등록일 최신순
+
+    switch (filter) {
+      case '낮은가격순':
+        sortOption = { price: 1 };
+        break;
+      case '높은가격순':
+        sortOption = { price: -1 };
+        break;
+      case '최신순': // 상품의 등록일 기준 최신순 (Product.createdAt)
+      default:
+        sortOption = { createdAt: -1 };
+        break;
+    }
+
+    const userLikedList = await this.likedProductsModel
+      .findOne({ uid: new Types.ObjectId(uid) })
+      .populate<{ productIds: Product[] }>({
+        path: 'productIds',
+        options: { sort: sortOption },
+      })
+      .exec();
+
+    if (
+      !userLikedList ||
+      !userLikedList.productIds ||
+      userLikedList.productIds.length === 0
+    ) {
+      return [];
+    }
+
+    return userLikedList.productIds;
+  }
+
+  /**
+   * 사용자가 최근 확인한 상품 정보를 filter조건에 맞게 반환합니다
+   * @param uid 사용자 uid
+   * @param filter 최신순(default), 낮은가격순, 높은가격순
+   */
+  async getViewedProduct(
+    uid: string,
+    filter: string = '최신순',
+  ): Promise<Product[]> {
+    let sortOption: Record<string, 1 | -1> = { createdAt: -1 }; // 기본값: 상품 등록일 최신순
+
+    switch (filter) {
+      case '낮은가격순':
+        sortOption = { price: 1 };
+        break;
+      case '높은가격순':
+        sortOption = { price: -1 };
+        break;
+      case '최신순': // 상품의 등록일 기준 최신순 (Product.createdAt)
+      default:
+        sortOption = { createdAt: -1 };
+        break;
+    }
+
+    const userLikedList = await this.likedProductsModel
+      .findOne({ uid: new Types.ObjectId(uid) })
+      .populate<{ productIds: Product[] }>({
+        path: 'productIds',
+        options: { sort: sortOption },
+      })
+      .exec();
+
+    if (
+      !userLikedList ||
+      !userLikedList.productIds ||
+      userLikedList.productIds.length === 0
+    ) {
+      return [];
+    }
+
+    return userLikedList.productIds;
   }
 }
