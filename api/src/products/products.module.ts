16c2ce6d2904b9d56eb17459305da963535e4d54
owtdxv <owtdxv@gmail.com>
2025-05-30 11:20:08 +0900

    feat: 사용자의 판매중,판매완료 상품 반환 service함수 생성

--- api/src/products/products.module.ts
+++ api/src/products/products.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from 'src/common/schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService, JwtStrategy],
 })
 export class ProductsModule {}
--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -1,4 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Product, ProductDocument } from 'src/common/schemas/product.schema';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+    private jwtService: JwtService,
+  ) {}
+
+  /**
+   * 사용자가 "판매중"인 상품을 filter조건에 맞게 반환합니다
+   * @param uid 사용자 uid
+   * @param filter 최신순(default), 낮은가격순, 높은가격순
+   */
+  async getSoldProduct(uid: string, filter: string = '최신순') {
+    let query = this.productModel.find({
+      sellerId: new Types.ObjectId(uid),
+      status: '판매중',
+    });
+
+    switch (filter) {
+      case '낮은가격순':
+        query = query.sort({ price: 1 });
+        break;
+      case '높은가격순':
+        query = query.sort({ price: -1 });
+        break;
+      case '최신순':
+      default:
+        query = query.sort({ createdAt: -1 });
+        break;
+    }
+
+    return await query.exec();
+  }
+
+  /**
+   * 사용자가 "판매완료"한 상품을 filter조건에 맞게 반환합니다
+   * @param uid 사용자 uid
+   * @param filter 최신순(default), 낮은가격순, 높은가격순
+   */
+  async getSoldOutProduct(uid: string, filter: string = '최신순') {
+    let query = this.productModel.find({
+      sellerId: new Types.ObjectId(uid),
+      status: '판매완료',
+    });
+
+    switch (filter) {
+      case '낮은가격순':
+        query = query.sort({ price: 1 });
+        break;
+      case '높은가격순':
+        query = query.sort({ price: -1 });
+        break;
+      case '최신순':
+      default:
+        query = query.sort({ createdAt: -1 });
+        break;
+    }
+
+    return await query.exec();
+  }
+}
