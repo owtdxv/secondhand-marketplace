643bf366b4e625c2bb7e73ec65f6898907ea5ba0
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-05-31 16:37:48 +0900

    feat : add pagenation

--- app/src/components/pageNation/PageNation.tsx
+++ app/src/components/pageNation/PageNation.tsx
@@ -1,7 +1,25 @@
-const PageNation = () => {
+import styles from "@/styles/components/pageNation.module.css";
+
+interface PropsType {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const PageNation = ({ totalPages, currentPage, onPageChange }: PropsType) => {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
   return (
-    <div>
-      <div></div>
+    <div className={styles.pagination}>
+      {pages.map((page) => (
+        <div
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={`${styles.dot} ${
+            currentPage === page ? styles.active : ""
+          }`}
+        ></div>
+      ))}
     </div>
   );
 };
--- app/src/pages/main/Main.tsx
+++ app/src/pages/main/Main.tsx
@@ -1,9 +1,45 @@
+import PageNation from "../../components/pageNation/PageNation";
+import Product from "../../components/product/Product";
 import styles from "../../styles/pages/main.module.css";
 
-const Main = () => {
+import arrowRightGray from "@/assets/icon/arrowRightGray.png";
+
+interface PropsType {
+  totalPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const Main = ({ totalPage, currentPage, onPageChange }: PropsType) => {
   return (
     <div className={styles.wrap}>
-      <div>메인페이지</div>
+      <div className={styles.mainWrap}>
+        <div className={styles.topAd}>
+          <div className={styles.wrapAd}>
+            <div className={styles.ad}></div>
+            <div className={styles.ad}></div>
+            <div className={styles.ad}></div>
+          </div>
+          <PageNation
+            totalPages={totalPage}
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+          />
+        </div>
+        <div className={styles.middleAd}></div>
+        <div>
+          <div>
+            <h1>최근 올라온 상품</h1>
+            <p>
+              더보기
+              <img width={17} src={arrowRightGray} />
+            </p>
+          </div>
+          <div>
+            <Product />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
