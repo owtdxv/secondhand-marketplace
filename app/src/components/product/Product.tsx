c3c1759c13a44b4a3001459dfecd442356a6b04d
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-05-31 18:28:11 +0900

    feat : publish MainPage

--- app/src/components/product/Product.tsx
+++ app/src/components/product/Product.tsx
@@ -1,7 +1,17 @@
-const Product = () => {
+import styles from "@/styles/components/product.module.css";
+import { ProductInfo } from "../../types/product";
+
+interface PropsType {
+  data: ProductInfo;
+}
+
+const Product = ({ data }: PropsType) => {
   return (
-    <div>
-      <div></div>
+    <div className={styles.wrap}>
+      <div className={styles.photo}>{data.photo}</div>
+      <div className={styles.title}>{data.title}</div>
+      <div className={styles.price}>{data.price}원</div>
+      <div className={styles.time}>{data.time}</div>
     </div>
   );
 };
--- app/src/pages/main/Main.tsx
+++ app/src/pages/main/Main.tsx
@@ -4,6 +4,8 @@ import Product from "../../components/product/Product";
 import styles from "@/styles/pages/main.module.css";
 
 import arrowRightGray from "@/assets/icon/arrowRightGray.png";
+import { ProductInfo } from "../../types/product";
+import data from "../../dummy-data/product.ts";
 
 interface PropsType {
   totalPage: number;
@@ -36,9 +38,64 @@ const Main = ({ totalPage, currentPage, onPageChange }: PropsType) => {
               <img width={17} height={17} src={arrowRightGray} />
             </div>
           </div>
-          <div>
-            <Product />
+          <div className={styles.wrapProducts}>
+            {data.map((item: ProductInfo) => (
+              <Product data={item} />
+            ))}
           </div>
+          <PageNation
+            totalPages={totalPage}
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+          />
+        </div>
+        <div className={styles.productsWrap} style={{ marginTop: "50px" }}>
+          <div className={styles.top}>
+            <div style={{ display: "flex" }}>
+              <div className={styles.title}>좋아요가 많은 상품</div>
+              <div className={styles.subTitle}>
+                사용자들이 많이 좋아요를 누른 상품을 보여드립니다.
+              </div>
+            </div>
+            <div className={styles.all}>
+              <div className={styles.text}>더보기</div>
+              <img width={17} height={17} src={arrowRightGray} />
+            </div>
+          </div>
+          <div className={styles.wrapProducts}>
+            {data.map((item: ProductInfo) => (
+              <Product data={item} />
+            ))}
+          </div>
+          <PageNation
+            totalPages={totalPage}
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+          />
+        </div>
+        <div className={styles.productsWrap} style={{ marginTop: "50px" }}>
+          <div className={styles.top}>
+            <div style={{ display: "flex" }}>
+              <div className={styles.title}>조회수가 많은 상품</div>
+              <div className={styles.subTitle}>
+                사용자들이 많이 조회한 상품을 보여드립니다.{" "}
+              </div>
+            </div>
+            <div className={styles.all}>
+              <div className={styles.text}>더보기</div>
+              <img width={17} height={17} src={arrowRightGray} />
+            </div>
+          </div>
+          <div className={styles.wrapProducts}>
+            {data.map((item: ProductInfo) => (
+              <Product data={item} />
+            ))}
+          </div>
+          <PageNation
+            totalPages={totalPage}
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+          />
         </div>
       </div>
       <Footer />
