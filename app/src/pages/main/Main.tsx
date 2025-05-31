fef0e641fe60f053d508c1da71e97252ec77bf12
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-05-31 17:47:17 +0900

    feat : publish footer

--- app/src/pages/main/Main.tsx
+++ app/src/pages/main/Main.tsx
@@ -1,6 +1,7 @@
+import Footer from "../../components/Footer";
 import PageNation from "../../components/pageNation/PageNation";
 import Product from "../../components/product/Product";
-import styles from "../../styles/pages/main.module.css";
+import styles from "@/styles/pages/main.module.css";
 
 import arrowRightGray from "@/assets/icon/arrowRightGray.png";
 
@@ -27,19 +28,20 @@ const Main = ({ totalPage, currentPage, onPageChange }: PropsType) => {
           />
         </div>
         <div className={styles.middleAd}></div>
-        <div>
-          <div>
-            <h1>최근 올라온 상품</h1>
-            <p>
-              더보기
-              <img width={17} src={arrowRightGray} />
-            </p>
+        <div className={styles.productsWrap}>
+          <div className={styles.top}>
+            <div className={styles.title}>최근 올라온 상품</div>
+            <div className={styles.all}>
+              <div className={styles.text}>더보기</div>
+              <img width={17} height={17} src={arrowRightGray} />
+            </div>
           </div>
           <div>
             <Product />
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
