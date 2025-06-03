9724a3fadfe8905532cbfa8aac370143c4fa193e
hyojin0326 <114805839+hyojin0326@users.noreply.github.com>
2025-06-03 18:09:21 +0900

    feat: 전체상품화면 페이지네이션, 필터링 처리

--- app/src/pages/productListPage/index.tsx
+++ app/src/pages/productListPage/index.tsx
@@ -5,10 +5,25 @@ import { useSearchParams } from "react-router-dom";
 
 const ProductListContainer = () => {
   const [products, setProducts] = useState([]);
-  const [searchParams] = useSearchParams();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const page = parseInt(searchParams.get("page") || "1");
   const filter = searchParams.get("filter") || "latest";
+
+  //페이지 변경시
+  const onPageChange = (page: number) => {
+    searchParams.set("page", page.toString());
+    setSearchParams(searchParams);
+  };
+  //필터 변경시
+  const onFilterChange = (newFilter: string) => {
+    searchParams.set("filter", newFilter);
+    searchParams.set("page", "1"); // 정렬 바꾸면 페이지 1로 초기화
+    setSearchParams(searchParams);
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -16,8 +31,10 @@ const ProductListContainer = () => {
         const res = await axios.get("/api/product", {
           params: { page, filter },
         });
-        console.log(res.data.items);
+        console.log(res.data);
         setProducts(res.data.items);
+        setCurrentPage(res.data.page);
+        setTotalPage(res.data.totalPages);
       } catch (error: any) {
         alert(
           "상품 조회 실패: " + (error.response?.data?.message || error.message)
@@ -28,7 +45,16 @@ const ProductListContainer = () => {
     fetchProducts();
   }, [page, filter]);
 
-  return <ProductListPage products={products} />;
+  return (
+    <ProductListPage
+      products={products}
+      currentPage={currentPage}
+      totalPage={totalPage}
+      onPageChanged={onPageChange}
+      onFilterChanged={onFilterChange}
+      currentFilter={filter}
+    />
+  );
 };
 
 export default ProductListContainer;
