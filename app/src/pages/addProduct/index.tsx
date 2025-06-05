8c215076f82ecbaec03999d18c789a925e432b4d
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-06-06 03:40:52 +0900

    feat : 상품 등록 페이지 퍼블리싱

--- app/src/pages/addProduct/index.tsx
+++ app/src/pages/addProduct/index.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import AddProduct from "./AddProduct";
 
 const AddProductContainer = () => {
-  return <AddProduct />;
+  const [modal, setModal] = useState<boolean>(false);
+
+  const modalHandler = () => {
+    setModal(!modal);
+  };
+  return <AddProduct modal={modal} modalHandler={modalHandler} />;
 };
 
 export default AddProductContainer;
