53c13b5a94c21d6ebb01f34d0163e2f9243de022
owtdxv <owtdxv@gmail.com>
2025-06-06 10:32:20 +0900

    feat: 판매자 정보 조회 API 완성

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get('/:uid')
+  getUserInfo(@Param('uid') uid: string) {
+    return this.userService.getUserInfo(uid);
+  }
+
+  @Get('/:uid/likes')
+  @UseGuards(AuthGuard('jwt'))
+  getUserLikesProduct(@Param('uid') uid: string, @Req() req) {}
+
+  @Get('/:uid/views')
+  @UseGuards(AuthGuard('jwt'))
+  getUserViewedProduct(@Param('uid') uid: string, @Req() req) {}
+}
--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { ProductsModule } from 'src/products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/common/schemas/user.schema';
+import { Product, ProductSchema } from 'src/common/schemas/product.schema';
+import { UserProductListSchema } from 'src/common/schemas/user-product-lists.schema';
 
 @Module({
-  providers: [UsersService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: User.name, schema: UserSchema },
+      { name: 'LikedProducts', schema: UserProductListSchema },
+      { name: 'ViewedProducts', schema: UserProductListSchema },
+    ]),
+    ProductsModule,
+  ],
+  providers: [UsersService, JwtStrategy],
   controllers: [UsersController],
 })
 export class UsersModule {}
