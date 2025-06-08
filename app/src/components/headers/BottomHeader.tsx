63148b0499f09bc4c9201b7e4858acb66bcc5fed
hyojin0326 <114805839+hyojin0326@users.noreply.github.com>
2025-06-08 20:13:23 +0900

    feat: 검생 퍼블 진행중

--- app/src/components/headers/BottomHeader.tsx
+++ app/src/components/headers/BottomHeader.tsx
@@ -4,17 +4,41 @@ import Logo from "@/assets/Logo.png";
 import chat from "@/assets/icon/chat.png";
 import myPage from "@/assets/icon/person.png";
 import sale from "@/assets/icon/sale.png";
+import { useState } from "react";
+import axios from "axios";
 
 const BottomHeader = () => {
+  const [showSearchDiv, setShowSearchDiv] = useState(false);
+  const [inputValue, setInputValue] = useState(false);
+
+  const clickSearchInput = () => {
+    setShowSearchDiv((prev) => !prev);
+  };
+
+  const searchProduct = async () => {
+    try {
+      const res = await axios.get("/api/product/search", {
+        params: { inputValue },
+      });
+      setShowSearchDiv(false);
+    } catch (err) {
+      console.error("상태 변경 실패", err);
+    }
+  };
   return (
     <div className={styles.wrap}>
       <div className={styles.header}>
         <img className={styles.logo} src={Logo} />
-        <input
-          className={styles.search}
-          type="text"
-          placeholder="상품명을 검색해보세요."
-        />
+        <div className={styles.search}>
+          <input
+            className={styles.searchInput}
+            type="text"
+            placeholder="상품명을 검색해보세요"
+            onFocus={() => setShowSearchDiv(true)}
+            onBlur={() => setTimeout(() => setShowSearchDiv(false), 150)}
+          />
+          {showSearchDiv && <div className={styles.searchDropdown}>adsf</div>}
+        </div>
         <ul className={styles.menu}>
           <li className={styles.item}>
             <img width={25} src={sale} />
