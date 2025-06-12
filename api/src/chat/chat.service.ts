c5a21870b85a9bcb105b6f73638ec09f574204ae
owtdxv <owtdxv@gmail.com>
2025-06-12 16:32:07 +0900

    fix: 채팅방이 이미 생성되어 있는 경우에도 채팅하기 버튼 클릭히 해당 채팅방으로 이동하도록 수정

--- api/src/chat/chat.service.ts
+++ api/src/chat/chat.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   ForbiddenException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
@@ -34,23 +35,43 @@ export class ChatService {
    * @returns 채팅방 ID
    */
   async createOrGetChatRoom(participants: string[]) {
+    // 참가자 배열의 유효성을 검사합니다.
     if (!Array.isArray(participants) || participants.length !== 2) {
       throw new BadRequestException('participants는 정확히 2명이어야 합니다.');
     }
 
+    // 참가자 ID를 ObjectId로 변환하고 오름차순으로 정렬하여 일관성을 유지합니다.
     const objectIds = participants.map((id) => new Types.ObjectId(id));
     objectIds.sort(); // 오름차순 정렬
 
+    // 정렬된 참가자 목록을 기준으로 기존 채팅방을 찾습니다.
     let chatRoom = await this.chatRoomModel.findOne({
       participants: objectIds,
     });
 
+    // 기존 채팅방이 없는 경우
     if (!chatRoom) {
       chatRoom = await this.chatRoomModel.create({
         participants: objectIds,
       });
+      console.log('새로운 채팅방이 생성되었습니다.');
+    } else {
+      chatRoom = await this.chatRoomModel.findOneAndUpdate(
+        { _id: chatRoom._id }, // 업데이트할 채팅방의 ID를 기준으로 찾습니다.
+        { $set: { isNewChatRoom: true } }, // isNewChatRoom 필드를 true로 설정합니다.
+        { new: true, runValidators: true }, // 업데이트된 문서를 반환하고, 업데이트 시 스키마 유효성 검사를 실행합니다.
+      );
+      console.log('기존 채팅방의 isNewChatRoom이 true로 업데이트되었습니다.');
     }
 
+    // chatRoom이 성공적으로 생성되거나 업데이트되었는지 확인합니다.
+    if (!chatRoom) {
+      throw new InternalServerErrorException(
+        '채팅방을 생성하거나 업데이트하는 데 실패했습니다.',
+      );
+    }
+
+    // 생성되거나 업데이트된 채팅방의 ID를 반환합니다.
     return { chatRoomId: chatRoom._id };
   }
 
