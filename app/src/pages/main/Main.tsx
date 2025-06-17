beb9e5c8c7f780b6918428ab3e799ebf51a862d6
hyojin0326 <114805839+hyojin0326@users.noreply.github.com>
2025-06-17 15:16:52 +0900

    feat: 광고 사진 추가

--- app/src/pages/main/Main.tsx
+++ app/src/pages/main/Main.tsx
@@ -2,6 +2,8 @@ import Footer from "../../components/Footer";
 import PageNation from "../../components/pageNation/PageNation";
 import Product from "../../components/product/Product";
 import styles from "@/styles/pages/main.module.css";
+import ad1 from "@/assets/ad_1.png";
+import ad2 from "@/assets/ad_2.png";
 
 import arrowRightGray from "@/assets/icon/arrowRightGray.png";
 import {
@@ -10,6 +12,7 @@ import {
   ProductInfo,
 } from "../../types/product";
 import { User } from "../../types/user";
+import { Link } from "react-router-dom";
 4;
 interface PropsType {
   recentData: getProductResponse;
@@ -37,8 +40,14 @@ const Main = ({
       <div className={styles.mainWrap}>
         <div className={styles.topAd}>
           <div className={styles.wrapAd}>
-            <div className={styles.ad}></div>
-            <div className={styles.ad}></div>
+            <div
+              className={styles.ad}
+              style={{ backgroundImage: `url(${ad1})` }}
+            ></div>
+            <div
+              className={styles.ad}
+              style={{ backgroundImage: `url(${ad2})` }}
+            ></div>
             <div className={styles.ad}></div>
           </div>
           {/* <PageNation
@@ -51,10 +60,10 @@ const Main = ({
         <div className={styles.productsWrap}>
           <div className={styles.top}>
             <div className={styles.title}>최근 올라온 상품</div>
-            <div className={styles.all}>
+            <Link to="/products" className={styles.all}>
               <div className={styles.text}>더보기</div>
               <img width={17} height={17} src={arrowRightGray} />
-            </div>
+            </Link>
           </div>
           <div className={styles.wrapProducts}>
             {recentData?.items.map((item: ProductInfo) => (
@@ -75,10 +84,6 @@ const Main = ({
                 사용자들이 많이 좋아요를 누른 상품을 보여드립니다.
               </div>
             </div>
-            <div className={styles.all}>
-              <div className={styles.text}>더보기</div>
-              <img width={17} height={17} src={arrowRightGray} />
-            </div>
           </div>
           <div className={styles.wrapProducts}>
             {topLikeData?.items.map((item: ProductInfo) => (
@@ -98,10 +103,6 @@ const Main = ({
               <div className={styles.subTitle}>
                 사용자들이 많이 조회한 상품을 보여드립니다.
               </div>
-            </div>
-            <div className={styles.all}>
-              <div className={styles.text}>더보기</div>
-              <img width={17} height={17} src={arrowRightGray} />
             </div>
           </div>
           <div className={styles.wrapProducts}>
